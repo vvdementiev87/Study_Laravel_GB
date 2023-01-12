ced6bfee47e13368c0fb641536dcabd4b2e65599
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2023-01-12 13:22:19 +0300

    Initial commit

---
+++ laravel/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', static function () {
+    return view('welcome');
+});
+
+Route::get('/info', static function () {
+    return view('info');
+});
+
+Route::get('/news/{id?}', static function (string|null $id=null) {
+    return view('news',["id"=>$id]);
+});
